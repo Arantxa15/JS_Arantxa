67fe6689d8eaf875b6f418f67f406aca564bd325
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-05-17 13:56:39 +0200

    17 de mayo

---
+++ Ejerciccio 1/script.js
@@ -0,0 +1,10 @@
+// Saludo personalizado
+// 1
+let nombre = "Arantxa";
+
+console.log( "Hola, "  + nombre);
+
+// 2
+let edad = 21;
+
+console.log("Felicidades " + nombre + ", ya tienes " + edad + "!");
---
+++ script.js
@@ -0,0 +1,29 @@
+// Empiece
+let nombre = "Arantxa";
+let edad = 21;
+
+console.log("Soy, " + nombre + " y tengo " + edad);
+
+// Operadores matemáticos
+
+// Suma +
+console.log(3+8);
+console.log(edad+8);
+
+// Resta -
+console.log(8-15);
+console.log(edad-15);
+
+// Multiplicaión *
+console.log(8 * 10);
+console.log(edad * 15);
+
+// División /
+console.log(8/9);
+console.log(edad/2);
+
+// % devuelve el resto de la división, cualquier número par dividido entre dos el resto es 0, si es impar el resto es 1.
+console.log(210%2);
+console.log(210/2);
+console.log(211%2);
+console.log(edad%2);
