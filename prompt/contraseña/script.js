9bd8fd44c974e315c299c2f63b99ba0f219aa5dd
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-05-21 13:51:53 +0200

    21 de mayo

---
+++ prompt/contraseña/script.js
@@ -0,0 +1,32 @@
+// Contraseña
+// let contraseñaAlmacenada = "abc";
+// let contraseñaIngresada = prompt("Ingresa contraseña");
+// let igualdad = contraseñaAlmacenada === contraseñaIngresada;
+
+// console.log("La contraseña es: " + igualdad);
+
+
+
+
+
+// Verificación de credenciales
+// let llueve = true; //Si llueve
+// let haceSol = false; //No hace sol
+
+// console.log(llueve && haceSol); // false
+// console.log(llueve || haceSol); // true	
+
+// Almacenadas
+let usuario = "arantxa";
+let contraseña = 152003;
+
+// Ingresado
+let inicio = prompt("Ingresar usuario")
+let pass = prompt("Ingresar contraseña")
+
+// Comparación
+let igual = (usuario == inicio) && (contraseña == pass);
+
+console.log("Los datos introducidos son: " + igual)
+
+
