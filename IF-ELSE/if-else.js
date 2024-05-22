2b04b2b0487b430a0bdf0d9417970905cb04c686
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-05-22 13:51:18 +0200

    22 de mayo

---
+++ IF-ELSE/if-else.js
@@ -0,0 +1,44 @@
+// IF ELSE
+
+let Edad = 18;
+
+if (Edad >= 18) {
+    console.log("Eres mayor de edad");
+    // si se cumple la condición
+} else {
+    console.log("Eres menor de edad");
+    // no se cumple la condición
+}
+
+// Verificación de número positivo o negativo
+
+let ingresarNumero = prompt("Ingresar número");
+
+if (ingresarNumero >= 0) {
+    console.log("El número es positivo");
+} else {
+    console.log("El número es negativo");
+}
+
+// Verificación de número par o impar
+
+let numero = prompt("Introducir número");
+
+if ((numero % 2) == 0 ){
+    console.log("El numero es par");
+} else {
+    console.log("El mumero es impar");
+}
+
+
+// Verificación de contraseña
+
+let contraseña = prompt("Ingrese una contraseña");
+let contraseñaAlmacenada = 190598;
+
+if (contraseñaAlmacenada == contraseña) {
+    console.log("La contraseña es correcta");
+} else {
+    console.log("La contraseña es incorrecta");
+
+}
--- prompt/contraseña/script.js
+++ prompt/contraseña/script.js
@@ -1,20 +1,20 @@
 // Contraseña
-// let contraseñaAlmacenada = "abc";
-// let contraseñaIngresada = prompt("Ingresa contraseña");
-// let igualdad = contraseñaAlmacenada === contraseñaIngresada;
+let contraseñaAlmacenada = "abc";
+let contraseñaIngresada = prompt("Ingresa contraseña");
+let igualdad = contraseñaAlmacenada === contraseñaIngresada;
 
-// console.log("La contraseña es: " + igualdad);
+console.log("La contraseña es: " + igualdad);
 
 
 
 
 
 // Verificación de credenciales
-// let llueve = true; //Si llueve
-// let haceSol = false; //No hace sol
+let llueve = true; //Si llueve
+let haceSol = false; //No hace sol
 
-// console.log(llueve && haceSol); // false
-// console.log(llueve || haceSol); // true	
+console.log(llueve && haceSol); // false
+console.log(llueve || haceSol); // true	
 
 // Almacenadas
 let usuario = "arantxa";
@@ -28,5 +28,23 @@ let pass = prompt("Ingresar contraseña")
 let igual = (usuario == inicio) && (contraseña == pass);
 
 console.log("Los datos introducidos son: " + igual)
+
+// Verificación de rango de edad /5.2
+// Edad del usuario
+let edad = prompt("Ingresar edad");
+let comprobación = (edad >= 18) && (edad <= 65);
+
+// Comprobación
+console.log("Tiene la edad permitida: " + comprobación);
+
+// 5.3
+let años = prompt("introduce tu edad");
+let cuentaActiva = true;
+let edadPermitida = (años >=18) && cuentaActiva;
+
+
+console.log("La verificación es: " + edadPermitida);
+
+
 
 
