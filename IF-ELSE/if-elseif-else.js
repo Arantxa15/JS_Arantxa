21d153ac54507fafe184624d6f9a609735034a9b
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-05-24 13:39:54 +0200

    24 de mayo

--- IF-ELSE/if-elseif-else.js
+++ IF-ELSE/if-elseif-else.js
@@ -53,10 +53,35 @@
 // } else {
 //     console.log("El número ingresado no es válido. Por favor, ingresa un número del 1 al 7.")
 // }
-   
+
 
 // 4
-let totalCompra = prompt("TOTAL");
+// let totalCompra = prompt("TOTAL");
+// let descuento = 0
 
-if (totalCompra);
+// if (totalCompra > 1000 + descuento){
+//     console.log("El descuento es " + (totalCompra * 0.10) + "€");
+// } else if (totalCompra > 500 + descuento) {
+//     console.log("El descuento es " + (totalCompra * 0.05) + "€");
+// } else (totalCompra <= 500 + descuento); {
+//     console.log("No se aplica ningún descuento");
+// } 
 
+
+
+// 5
+let nombreMes = prompt("Ingrese un mes en minúsculas");
+let dias;
+
+if (nombreMes === "enero" || nombreMes === "marzo" || nombreMes === "mayo" || nombreMes === "julio" || nombreMes === "agosto" || nombreMes === "octubre" || nombreMes === "diciembre") {
+    dias = 31;
+    console.log("El mes de" , nombreMes, "tiene", dias, "días.");
+} else if (nombreMes === "febrero") {
+    dias = 28;
+    console.log("El mes de" , nombreMes, "tiene", dias, "días.")
+} else if (nombreMes === "abril" || nombreMes === "junnio" ||nombreMes === "septiembre" || nombreMes === "noviembre") {
+    dias = 30;
+    console.log("El mes de" , nombreMes, "tiene", dias, "días.")
+} else {
+    console.log("El nombre del mes no es válido");
+}
