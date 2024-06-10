a90da704d0ee08391e81727f3afd7e3cab5bf7ee
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-06-10 13:04:50 +0200

    10 de junio

---
+++ REPASO DE EXAMEN/array-repaso.js
@@ -0,0 +1,65 @@
+console.log('EJERCICIO 1');
+// Agregar elementos a un array vacío
+
+// - Crea un array vacío
+
+let listaFrutas = [];
+// - Agrega, Manzana, Banana y Naranja al array vacío.
+
+console.log(listaFrutas);
+listaFrutas.push('manzana', 'banana', 'naranja')
+
+console.log('EJERCICIO 2');
+// Agregar elementos a un array que ya existe.
+
+// - Crea un array que contenga 1,2,3
+
+let numeros = [
+    1, 2, 3
+];
+// - Agrega números hasta el 6
+
+numeros.push(4, 5, 6);
+console.log(numeros);
+
+
+
+console.log('EJERCICIO 3');
+// Tienes un array inicial con los valores 1,2,3,4,5. 
+
+let listaNumeros = [
+    1, 2, 3, 4, 5
+];
+
+// Quieres cambiar el número en la posición 2 por el número 10. 
+listaNumeros[1] = 10;
+console.log(listaNumeros);
+
+
+
+console.log('EJERCICIO 4');
+
+// Suma de elementos en un array utilizando una función.
+
+// - Tienes un array de números y quieres calcular la suma de todos los valores del array. 
+let sumaNumeros = [
+    10, 20, 30, 40
+];
+
+function suma(sumaNumeros) {
+    return  sumaNumeros(10 + 20 + 30 + 40);
+
+}
+console.log(sumaNumeros)
+// - Crea una función que devuelva la suma de los números.
+// - Recorre el array y suma cada valor.
+
+
+
+
+
+console.log('EJERCICIO 5');
+
+// 5 Ingresar 3 valores que meta el usuario a un array, utilizando una función.
+
+// Declaramos un array vacío.
