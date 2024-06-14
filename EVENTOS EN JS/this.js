c20e39cdadc1cf183712a4ba4e5a43d8e3dd1880
Arantxa15 <arantxa.gambarte15@gmail.com>
2024-06-14 13:49:17 +0200

    14 de junio

--- EVENTOS EN JS/this.js
+++ EVENTOS EN JS/this.js
@@ -3,11 +3,34 @@
 let fondo = document.getElementById('fondo');
 let divs = document.querySelectorAll('.miDiv');
 
+// 1ª opción
 function cambiarColor() {
     fondo.style.backgroundColor = this.style.backgroundColor;
 }
+
+// 2º opcion
+// for(let i = 0; i < divs.length; i++){ divs[i].addEventListener("mosueover", cambiarColor)}
 
 divs[0].addEventListener("mouseover", cambiarColor);
 divs[1].addEventListener("mouseover", cambiarColor);
 divs[2].addEventListener("mouseover", cambiarColor);
 divs[3].addEventListener("mouseover", cambiarColor);
+
+
+
+// 5- Tengo una imagen de 300px ancho, cuando pase el ratón por encima la imagen se cambiara por otro diferente, al quitar el ratón la imagen volverá a aparecer.
+let imagen = document.getElementById('perro')
+let imagenOriginal = "../EVENTOS EN JS/imagenes/pastor_belga.jpg"
+let imagenNueva = "../EVENTOS EN JS/imagenes/golden.jpg"
+
+function cambiarImagen() {
+imagen.src = imagenNueva;
+    
+}
+imagen.addEventListener("mouseover", cambiarImagen);
+
+function restaurarImagen(){
+    imagen.src = imagenOriginal;
+ 
+}
+imagen.addEventListener("mouseout", restaurarImagen);
---
+++ REPASO DE EXAMEN/innerHTML/innerHTML.js
@@ -0,0 +1,19 @@
+// Acceder al contenido HTML dentro del div con id "miDiv"
+// let contenido = document.getElementById('miP').innerHTML;
+
+// Modificar el contenido HTML dentro del div
+// document.getElementById('miP').innerHTML  = 'Nuevo <strong>contenido</strong> para el párrafo';
+
+
+// 1- Cambiar el contenido de un elemento utilizando innerHTML
+let innerHTML = document.getElementById('miP').innerHTML;
+
+document.getElementById('miP').innerHTML = 'Este es el nuevo contenido utilizando <strong>innerHTML</strong>.';
+
+// 2- Cambiar el contenido de un elemento utilizando textContent
+let textContent = document.getElementById('miP2').textContent;
+
+document.getElementById('miP2').textContent = 'Este es el nuevo contenido utilizando textContent.'
+
+
+// 3- Crea una página web con un botón y un div vacío. Utiliza JavaScript para que, al hacer clic en el botón, el contenido del div cambie a “Has hecho click en el botón”
